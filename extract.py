caf68dd38f95db35cde613da9a0e38658f9f612c
Tyler Trahan <tyler@tylertrahan.com>
2023-08-01 10:00:36 -0400

    Cleanup: Refactor image extractor

--- extract.py
+++ extract.py
@@ -9,11 +9,59 @@ from pptx.enum.shapes import MSO_SHAPE_TYPE
 import glob
 import csv
 
-def iter_picture_shapes(prs):
-    for slide in prs.slides:
+_image_index = 0
+_errors = 0
+
+def save_image(image, name):
+    '''
+    Save an image to disk.
+
+    :param image The image to save.
+    :param name The partial name of the image, including the directory but not including the sequence number.
+    '''
+    global _image_index
+
+    image_bytes = image.blob
+
+    # Append the sequence number and file extension to the partial name
+    name = name + ' ' + str(_image_index) + '.' + image.ext
+
+    print(name)
+    with open(name, 'wb') as f:
+        f.write(image_bytes)
+
+    _image_index += 1
+
+def drill(shape, name):
+    '''
+    Recursive function to look inside grouped shapes for pictures and save them.
+
+    :param shape The parent shape to look inside.
+    :param name The partial name for any images we find, including the directory but not including the sequence number.
+    '''
+    global _image_index
+    global _errors
+
+    if shape.shape_type == MSO_SHAPE_TYPE.GROUP:
+        for s in shape.shapes:
+            drill(s, name)
+    if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
+        try:
+            save_image(shape.image, name)
+        except:
+            print("Could not process an image")
+            _errors += 1
+
+def iter_shapes(p):
+    '''
+    Iterate through shapes in the given Presentation
+
+    :param p The presentation to iterate through.
+    '''
+    for slide in p.slides:
         for shape in slide.shapes:
-            if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
-                yield shape
+            yield shape
+
 
 def process_images(input_dir, output_dir):
     """
@@ -22,29 +70,25 @@ def process_images(input_dir, output_dir):
     :param input_dir The name of the subfolder to search for PowerPoint documents.
     :param output_dir The name of the subfolder in which to save images.
     """
+    global _image_index
 
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
     for eachfile in glob.glob(input_dir + os.sep + "*.pptx"):
-        image_number = 1
+        _image_index = 1
 
-        for picture in iter_picture_shapes(Presentation(eachfile)):
-            image = picture.image
-            image_bytes = image.blob
+        # Strip input directory path and replace with chosen output path
+        name = eachfile.split(os.sep)[1]
 
-            # Strip directory path and replace with chosen output path
-            image_filename = eachfile.split(os.sep)[1]
-            image_filename = output_dir + os.sep + image_filename
+        # Strip file extension
+        name = name.split('.')[0]
 
-            # Name the image sequentially
-            image_filename = image_filename + ' ' + str(image_number) + '.' + image.ext
+        # Prepend output directory to name, for saving later
+        name = output_dir + os.sep + name
 
-            print(image_filename)
-            with open(image_filename, 'wb') as f:
-                f.write(image_bytes)
-
-            image_number += 1
+        for shape in iter_shapes(Presentation(eachfile)):
+            drill(shape, name)
 
 def process_text(input_dir):
     """
@@ -80,7 +124,7 @@ def process_text(input_dir):
                 # Write the page number, collected text, and presenter notes as a new row
                 writer.writerow([eachfile, page, text, slide.notes_slide.notes_text_frame.text])
 
-        print("Finished. Total files: " + str(count))
+        print("Finished. Total files: " + str(count) + ", total errors reading images: " + str(_errors))
 
 def main():
     input_dir = 'input'
